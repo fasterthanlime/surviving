637d4feee7521be3d9bd3b8206dd40777c721741
Amos Wenger <amoswenger@gmail.com>
2020-08-09 15:07:38 +0200

    send troubles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use argh::FromArgs;
 use async_std::{fs::File, io::ReadExt};
 
@@ -22,7 +24,7 @@ async fn main() -> Result<(), eyre::Error> {
 
     for file in &args.files {
         let file = file.clone();
-        let handle = async_std::task::spawn(async move {
+        let handle = async_std::task::spawn_local(async move {
             let res = hash_file(&file).await;
             if let Err(e) = res {
                 println!("While hashing {}: {}", file.display(), e);
@@ -39,12 +41,14 @@ async fn main() -> Result<(), eyre::Error> {
 }
 
 async fn hash_file(path: &Path) -> Result<(), eyre::Error> {
-    let mut file = File::open(path).await?;
+    let file = File::open(path).await?;
+    let mut file = TracingReader { inner: file };
+
     let mut hasher = sha3::Sha3_256::new();
 
     let mut buf = vec![0u8; 256 * 1024];
     loop {
-        let n = file.read(&mut buf[..]).await?;
+        let n = Pin::new(&mut file).simple_read(&mut buf[..]).await?;
         match n {
             0 => break,
             n => hasher.update(&buf[..n]),
@@ -59,4 +63,64 @@ async fn hash_file(path: &Path) -> Result<(), eyre::Error> {
     println!();
 
     Ok(())
+}
+
+use futures::io::AsyncRead;
+use std::{
+    io,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use pin_project::pin_project;
+
+// Generate projection types
+#[pin_project]
+struct TracingReader<R>
+where
+    R: AsyncRead,
+{
+    // pinning is structural for `inner`
+    #[pin]
+    inner: R,
+}
+
+impl<R> AsyncRead for TracingReader<R>
+where
+    R: AsyncRead,
+{
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        // tracing
+        let address = &self as *const _;
+        println!("{:?} => {:?}", address, std::thread::current().id());
+
+        // reading
+        self.project().inner.poll_read(cx, buf)
+    }
+}
+
+use async_trait::async_trait;
+
+#[async_trait]
+trait SimpleRead {
+    async fn simple_read(self: Pin<&mut Self>, buf: &mut [u8]) -> io::Result<usize>;
+}
+
+#[async_trait]
+impl<R> SimpleRead for TracingReader<R>
+where
+    R: AsyncRead + Send,
+{
+    async fn simple_read(self: Pin<&mut Self>, buf: &mut [u8]) -> io::Result<usize> {
+        // tracing
+        let address = &self as *const _;
+        println!("{:?} => {:?}", address, std::thread::current().id());
+
+        // reading
+        self.project().inner.read(buf).await
+    }
 }
